1c99594686ba2d9dc57c31a6436ac27b95400161
Piyush Sarkar <hi.mrpiyush@gmail.com>
2025-12-23 13:56:27 +0530

    Add logo and GitHub link to header
    Updated the Header component to display a logo image next to the app name and added a GitHub icon linking to an external profile. This enhances branding and provides quick access to the project's GitHub page.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { Github } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import ThemeToggleButton from "../ThemeToggleButton";
 
@@ -7,7 +9,16 @@ const Header = () => {
 			className="fixed right-0 left-0 border-b shadow"
 			aria-label="app-header">
 			<div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-3">
-				<Link href={"/"}>
+				<Link
+					href={"/"}
+					className="flex items-center gap-2">
+					<Image
+						src="/logo.png"
+						alt="Logo"
+						width={62}
+						height={62}
+						className="object-contain"
+					/>
 					<h1
 						className="text-2xl font-semibold"
 						aria-label="App Name">
@@ -17,7 +28,12 @@ const Header = () => {
 
 				<nav className="flex items-center gap-4">
 					<Link href={"/"}>Home</Link>
-
+					<Link
+						href={"https://github.com/piyushsarkar-dev"}
+						target="_blank"
+						rel="noopener noreferrer">
+						<Github />
+					</Link>
 					<ThemeToggleButton />
 				</nav>
 			</div>
